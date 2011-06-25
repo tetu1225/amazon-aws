e8d3ed2b9517e76aad6a98af59a42c42f4d18228
Fujiki Taiju <fujitaiju@gmail.com>
2011-06-25 10:25:34 +0900

    テンプレート変更

--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
 require 'rubygems'
 require 'sinatra'
-require 'amazon/aws'
+require 'amazon/aws/search'
 require 'haml'
+
+include Amazon::AWS
+include Amazon::AWS::Search
 
 get '/' do
 	@var = "Hello Haml!"
 	haml :index
 end
 
-get '/item_search' do
+get '/item_search/:keywords' do
+	is = ItemSearch.new('Books', {'Keywords' => :keywords})
+	is.response_group = ResponseGroup.new('Small')
+	req = Request.new
+	req.locale = 'jp'
+	
+	response = req.search(is)
+	items = response.item_search_response[0].items[0].item
 	haml :item_search
 end
